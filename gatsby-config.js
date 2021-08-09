a4aa8d313130d6887723455e0bfabe095cae6f32
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-09 17:53:49 +0900

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
